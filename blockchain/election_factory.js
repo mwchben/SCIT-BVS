93a56df9652acd4c7e00978945c5168f8b442b4f
Benjamin Chege <benjamwa05gmail.com>
2023-08-07 14:34:06 +0300

    changes in json

--- blockchain/election_factory.js
+++ blockchain/election_factory.js
@@ -6,6 +6,7 @@ const instance = new web3.eth.Contract(
 	JSON.parse(ElectionFactory.interface),
     process.env.CONTRACT_ADDRESS
     // '0x73F951a06Eb211B9d877F316c79dFc7e7ad3ae42'
+    // now -> '0x2989c96510C248EE0603fd36480C722E30d52898'
 );
 
 instance.options.address = process.env.CONTRACT_ADDRESS;
